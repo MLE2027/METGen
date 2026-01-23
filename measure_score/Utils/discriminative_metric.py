dde2805b2190f344283b5bd053b56fc505838848
IMPAC <Lcb_ds2027@163.com>
2026-01-23 19:16:58 +0800

    Update discriminative_metric.py

--- measure_score/Utils/discriminative_metric.py
+++ measure_score/Utils/discriminative_metric.py
@@ -44,7 +44,6 @@ def discriminative_score_metrics(ori_data, generated_data):
     iterations = 3000
     batch_size = 128
 
-    # ----- TF 2.x 模型定义 -----
     class Discriminator(tf.keras.Model):
         def __init__(self, hidden_dim):
             super().__init__()
@@ -55,7 +54,6 @@ def discriminative_score_metrics(ori_data, generated_data):
             self.logit = tf.keras.layers.Dense(1, activation=None, name='d_logit')
 
         def call(self, x, training=None):
-            # x: (batch, time, dim)  →  GRU 只返回最后时刻输出
             gru_out = self.gru(x, training=training)
             return self.logit(gru_out)
 
@@ -65,11 +63,9 @@ def discriminative_score_metrics(ori_data, generated_data):
     bce_fake = tf.keras.losses.BinaryCrossentropy(from_logits=True)
     optimizer = tf.keras.optimizers.Adam()
 
-    # ----- 数据划分 -----
     train_x, train_x_hat, test_x, test_x_hat, train_t, train_t_hat, test_t, test_t_hat = \
         train_test_divide(ori_data, generated_data, ori_time, gen_time)
 
-    # ----- 训练 -----
     @tf.function
     def train_step(x_real, x_fake):
         with tf.GradientTape() as tape:
@@ -86,7 +82,7 @@ def discriminative_score_metrics(ori_data, generated_data):
     for itt in tqdm(range(iterations), desc='training', total=iterations):
         x_mb, t_mb = batch_generator(train_x, train_t, batch_size)
         x_hat_mb, t_hat_mb = batch_generator(train_x_hat, train_t_hat, batch_size)
-        # 按原始时间长度做动态长度掩码（与 TF1 版行为一致）
+        # 按原始时间长度做动态长度掩码
         x_real = tf.keras.preprocessing.sequence.pad_sequences(
             x_mb, maxlen=max_seq_len, dtype='float32', padding='post')
         x_fake = tf.keras.preprocessing.sequence.pad_sequences(
@@ -95,7 +91,6 @@ def discriminative_score_metrics(ori_data, generated_data):
         if itt % 500 == 0:
             print('Loss: {}'.format(step_d_loss.numpy()))
 
-    # ----- 测试 -----
     def predict(x_list):
         x_pad = tf.keras.preprocessing.sequence.pad_sequences(
             x_list, maxlen=max_seq_len, dtype='float32', padding='post')
